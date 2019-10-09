a4c8a5bf4e2bc826598e3c2e6d2c609b2459e8a6
Pepe <mar15163@uvg.edu.gt>
2019-10-09 12:55:18 -0600

    Limpieza

--- lab6/main.R
+++ lab6/main.R
@@ -9,6 +9,34 @@
 data<-read.csv("GrammarandProductReviews.csv")
 View(head(data,10))
 
+#----------------- LIMPIEZA -----------------#
+# To lower
+data$reviews.text <- tolower(data$reviews.text)
+data$reviews.title <- tolower(data$reviews.title)
+
+# reemplazar caracteres
+data$reviews.text <- gsub("@", "", data$reviews.text)
+data$reviews.text <- gsub("#", "", data$reviews.text)
+data$reviews.text <- gsub("'", "", data$reviews.text)
+
+data$reviews.title <- gsub("@", "", data$reviews.title)
+data$reviews.title <- gsub("#", "", data$reviews.title)
+data$reviews.title <- gsub("'", "", data$reviews.title)
+
+# Quitar stopwords, signos de puntuacion
+stopWords <- stopwords(kind = "en")
+
+data$reviews.text <- removeWords(data$reviews.text, stopWords)
+data$reviews.text <- removePunctuation(data$reviews.text)
+data$reviews.text <- removeNumbers(data$reviews.text)
+
+data$reviews.title <- removeWords(data$reviews.title, stopWords)
+data$reviews.title <- removePunctuation(data$reviews.title)
+data$reviews.title <- removeNumbers(data$reviews.title)
+
+# Quitar urls
+data$reviews.text<- gsub('http\\S+\\s*', '', data$reviews.text)
+
 install.packages("sentimentr")
 library(sentimentr)
 
