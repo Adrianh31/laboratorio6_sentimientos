b74a43bc1086faa293beb7070a8dbeef92cc10ae
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2019-10-09 12:28:07 -0600

    sentiment analysis working
    library sentimentr

--- lab6/main.R
+++ lab6/main.R
@@ -1,7 +1,35 @@
 # Universidad del Valle de Guatemala
 # Lab 6
 
-# Jose Pablo Viana 16
-# Jose Martinez 15163
+# Jose Pablo Viana  16091
+# Jose Martinez     15163
+# Sergio Marchena   16387
 
-data <- read.csv("../GrammarandProductReviews.csv")
+
+data<-read.csv("GrammarandProductReviews.csv")
+View(head(data,10))
+
+install.packages("sentimentr")
+library(sentimentr)
+
+testData<-data
+
+testData$reviews.text<-as.character(testData$reviews.text)
+
+hola<-subset(testData[c(10,16,20,21)])
+hola$reviews.title<-as.character(hola$reviews.title)
+
+View(head(hola,10))
+
+hola<-na.omit(hola)
+
+text<-hola[hola$reviews.id == 148314686, "reviews.text"]
+text2<-hola[hola$reviews.id == 148314686, "reviews.title"]
+
+sentiment_by(text, var = NULL)
+sentiment(text)
+extract_sentiment_terms(text)
+
+sentiment_by(text2, var = NULL)
+sentiment(text2)
+extract_sentiment_terms(text2)
