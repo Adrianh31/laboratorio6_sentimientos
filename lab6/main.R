04043a344ad18327e1ce4ea5df62cb71bebd2f61
Pablo Viana <via16091@uvg.edu.gt>
2019-10-09 13:37:23 -0600

    primer acercamiento

--- lab6/main.R
+++ lab6/main.R
@@ -1,7 +1,41 @@
 # Universidad del Valle de Guatemala
 # Lab 6
+# Data science
+# Catedrática: Lynette García
+# Jose Pablo Viana  16091
+# Jose Martinez     15163
+# Sergio Marchena   16387
 
-# Jose Pablo Viana 16
-# Jose Martinez 15163
+#Enlace para descripcion de columnas: https://developer.datafiniti.co/docs/product-data-schema
 
-data <- read.csv("../GrammarandProductReviews.csv")
+install.packages("sentimentr")
+library(sentimentr)
+
+#Cargamos reviews
+data<-read.csv("GrammarandProductReviews.csv")
+#View(head(data,10))
+
+# ----------------------------- Análisis exploratorio ----------------------------
+nombre_y_review <- data[,c("name","reviews.title","reviews.text")]
+
+testData<-data
+
+testData$reviews.text<-as.character(testData$reviews.text)
+
+hola<-subset(testData[c(10,16,20,21)])
+hola$reviews.title<-as.character(hola$reviews.title)
+
+View(head(hola,10))
+
+hola<-na.omit(hola)
+
+text<-hola[hola$reviews.id == 148314686, "reviews.text"]
+text2<-hola[hola$reviews.id == 148314686, "reviews.title"]
+
+sentiment_by(text, var = NULL)
+sentiment(text)
+extract_sentiment_terms(text)
+
+sentiment_by(text2, var = NULL)
+sentiment(text2)
+extract_sentiment_terms(text2)
